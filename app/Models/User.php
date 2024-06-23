5aaf02e3541a7e00017482bf202fecc61f8fbbcd
mohamedelshahed <mohamed.hussein151515@gmail.com>
2024-06-23 21:08:47 +0300

    added Lesson, Tag models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function lessons()
+    {
+        return $this->hasMany(Lesson::class);
+    }
 }
